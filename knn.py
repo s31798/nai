1d4c123c394c8d4857062796f0bf26c671cd0a97
s31798 <s31798@pjwstk.edu.pl>
2025-03-10 21:26:03 +0100

    loading data from files

--- knn.py
+++ knn.py
@@ -1,7 +1,6 @@
+import heapq
 
-
-x_test = [5.0,3.2,1.2,0.2]
-
+from unicodedata import numeric
 
 
 class Model:
@@ -44,10 +43,45 @@ class Model:
 
     def open_test_data(self, file_name):
         self.x_test, self.y_test = Model.load_data_from_file(file_name)
+    def predict_at(self, index):
+        distances = Model.calculate_distances(m.x_train, m.x_test[index])
+        #indeces of k smallest values in O(nlogk)
+        index_neighbor_pairs = heapq.nsmallest(self.k, enumerate(distances), key=lambda x: x[1])
+        neighbors_index = [index for index, value in index_neighbor_pairs]
+        labels = {}
+        for neighbor in neighbors_index:
+            label = m.y_train[neighbor]
+            labels[label] = labels.get(label,0) + 1
+        count = 0
+        max_label = ""
+        for entry in labels.items():
+            if entry[1] > count:
+                count = entry[1]
+                max_label = entry[0]
+        return max_label
+    def predict_all(self):
+        result = []
+        for i in range(len(self.x_test)):
+            result.append(self.predict_at(i))
+        return result
+
+    def get_accuracy(self):
+        predictions = self.predict_all()
+        correct = 0
+        for i in range(len(self.y_test)):
+            if self.y_test[i] == predictions[i]:
+                correct += 1
+        accuracy = correct / len(self.y_test)
+        return accuracy
 
 
-m = Model(5)
+
+
+
+
+
+m = Model(4)
 m.open_training_data("iris.data")
 m.open_test_data("iris.test.data")
-print(Model.calculate_distances(m.x_train, x_test))
+print(m.get_accuracy())
 
